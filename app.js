5f88b9063e90e4089b541c0c0a34f53e6a836488
c7b <leckam7@gmail.com>
2023-04-24 20:27:17 -0600

    s

--- app.js
+++ app.js
@@ -158,6 +158,8 @@ app.get('/download', (req, res) => {
 });
 
 // Start the server
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
